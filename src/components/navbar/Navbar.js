c11f26d13bd2ecb9c01305dd6ad74fafb2b91490
Ouraborus <1222eca@gmail.com>
2018-03-08 22:27:06 -0500

    Uploading work with NavBar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,50 @@
-import React from 'react'
-
-export default function Navbar ({data}) {
-  console.log(data)
+import React, { Fragment } from 'react'
+import './_navbar.scss'
+export default function Navbar ({ data }) {
   return (
     // finish navbar structure with simpleLinks and composedLinks
+    <Fragment><nav className='navbar'>
+      <button className='navbar__icon'>
+        <span />
+        <span />
+        <span />
+      </button>
+      <img src={data.logo.link} alt={data.logo.img} />
+      <div className='navbar__list'>
+        <ul className='navbar__list-container'>
+          {data.links.forEach(element => {
+            if (element.hasOwnProperty('href')) {
+              return (
+                <li className='navbar__link'>
+                  <b>
+                    <a href={element.href}> {element.label}
+                    </a>
+                  </b>
+                </li>
+              )
+            } else {
+              return (<li className='navbar__link js-dropdown'>
+                <b>
+                  {element.label}
+                </b>
+                <ul className='navbar__dropdown navbar__dropdown--collapsed'>
+                  {data.links.forEach(subelement => {
+                    return (
+                      <li className='navbar__link'>
+                        <b>
+                          <a href={subelement.href}> {subelement.label}</a>
+                        </b>
+                      </li>)
+                  })}
+                </ul>
+              </li>
+              )
+            }
+          })}
+        </ul>
+
+      </div>
+    </nav>
+    </Fragment>
   )
 }
